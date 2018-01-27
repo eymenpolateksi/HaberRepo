119ec83e91f9ed1bf843b4acd7aed4c929d68fc2
eymenpolateksi <33779498+eymenpolateksi@users.noreply.github.com>
2018-01-27 15:27:03 +0300

    Update Haber.java

--- finalProject/src/main/java/com/eymeneksi/finalProject/Haber.java
+++ finalProject/src/main/java/com/eymeneksi/finalProject/Haber.java
@@ -1,17 +1,35 @@
 package com.eymeneksi.finalProject;
-
+/**
+ * Bu sınıfta bir haber nesnesinin bilgileri tutulur.
+ * @author Eymen Polat Ekşi 
+ */
 public class Haber {
 	private int id;
     private static int sayac=0;
     public String haberIcerik;
+	/**
+     * Bu fonksiyon sınıfımızın constructor'ı.
+     * Haber nesnesi bu fonksiyondan yapılır.
+     * @param haberIcerik haber metnini tutan deger.
+     */
     public Haber(String haberIcerik) {
     	super();
     	this.id=++sayac;
     	this.haberIcerik=haberIcerik;
     }
+	/**
+     * Başka bir sınıfta private metodların
+     * kullanılabilmesi için Getter
+     * @return haberIcerik
+     */
     public String gethaberIcerik() {
     	return haberIcerik;
     }
+	/**
+     * Başka bir sınıfta private degiskenlerin
+     * kullanılabilmesi için Getter
+     * @return id
+     */
     public int getId() {
     	return id;
     }
